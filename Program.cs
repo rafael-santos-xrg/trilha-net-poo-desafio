04377cc050a0fe0f41169da2b30d7c5314d78675
victor-santos-xrg <victorsantos.pba@outlook.com>
2024-10-10 18:31:52 -0300

    FEAT: Tests created

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+var meuIphone = new Iphone(numero: "31999999999", imei: "356303488900992", modelo: "15 Pro Max", memoria: 256);
+var meuNokia = new Nokia(numero: "31995196795", imei: "353612655606601", modelo: "X30", memoria: 128);
+
+meuIphone.Ligar();
+meuNokia.Ligar();
+
+meuIphone.ReceberLigacao();
+meuNokia.ReceberLigacao();
+
+meuIphone.InstalarAplicativo("iFood");
+meuNokia.InstalarAplicativo("FreeFire");
+
